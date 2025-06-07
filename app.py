565265c8c3fa31d46f2c6201c307a5bd87d429a7
asharrafli <muhamad21ti@mahasiswa.pcr.ac.id>
2025-06-07 15:12:48 +0700

    commit python

--- app.py
+++ app.py
@@ -3,3 +3,15 @@ import cv2
 import numpy as np
 from io import BytesIO
 
+# sketch effect 
+def sketch(frame):
+    # mengubah gambar ke grayscale terlebih dahulu
+    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # membersihkan noise dengan Gaussian blur
+    frame_blur= cv2.GaussianBlur(frame_gray,(5,5),0)
+    # emplementasi deteksi tepi dengan Canny
+    edges = cv2.Canny(frame_blur,30,60)
+    # do an inverse binary thresholding
+    ret, mask = cv2.threshold(edges, 240, 255, cv2.THRESH_BINARY_INV)
+
+    return mask
